de73bd26a9e4a5d03bbee1604fe207ef11c2d0e0
Mary Louise Hermosa <jayzces@gmail.com>
2020-03-16 17:17:33 +0800

    Add heroes view

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 
+import { Hero } from '../hero';
+import { HEROES } from '../mock-heroes';
+
 @Component({
   selector: 'app-heroes',
   templateUrl: './heroes.component.html',
@@ -7,10 +10,22 @@ import { Component, OnInit } from '@angular/core';
 })
 
 export class HeroesComponent implements OnInit {
+  hero = {
+    id: 1,
+    name: 'Windstorm'
+  };
+
+  heroes = HEROES;
+
+  selectedHero: Hero;
 
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  onSelect(hero: Hero): void {
+    this.selectedHero = hero;
   }
 
 }
